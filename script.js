a91d392e49fdffda3dee7924d77cb8ab7a6dad0c
Laura Rocha Prado <lauraprado@asu.edu>
2020-12-15 12:33:26 -0700

    removed unused code

--- script.js
+++ script.js
@@ -19,20 +19,12 @@ const fieldProps = [
 const fieldDiv = document.getElementById('fields');
 fieldProps.forEach((field) => {
   let li = document.createElement('li');
-  // let fieldElement = document.createElement('BUTTON');
-  // fieldElement.innerHTML = field.name;
   li.innerHTML = field.name;
   li.id = field.id;
   if (field.block === 'labelBlock') {
     li.draggable = 'true';
     li.classList.add('draggable');
-    // li.appendChild(fieldElement);
     fieldDiv.appendChild(li);
-  } else {
-    // let fixedDiv = document.getElementById(`label-${field.id}`);
-    // console.log(field.id);
-    // console.log(fixedDiv);
-    // fixedDiv.appendChild(li);
   }
 });
 
@@ -85,7 +77,6 @@ formatsArr.forEach((format) => {
   let btn = document.createElement('button');
   btn.classList.add('control');
   btn.disabled = true;
-  // btn.innerText = format.icon;
   btn.dataset.func = format.func;
   let icon = document.createElement('span');
   icon.classList.add('material-icons');
@@ -96,7 +87,6 @@ formatsArr.forEach((format) => {
 
 // Creates formatting (dropdown) controls in page
 dropdownsArr.forEach((dropObj) => {
-  // console.log('inside dropdownsArr each');
   let slct = document.createElement('select');
   slct.classList.add('control');
   slct.name = dropObj.name;
@@ -109,7 +99,6 @@ dropdownsArr.forEach((dropObj) => {
     slct.appendChild(opt);
   });
   controlDiv.appendChild(slct);
-  // console.log(dropObj);
 });
 
 // Grabs elements
@@ -122,11 +111,9 @@ const inputs = document.querySelectorAll('input');
 
 // Adds line (fieldBlock)
 function addLine() {
-  // console.log('clicked');
   let line = document.createElement('div');
   line.classList.add('field-block', 'container');
   let midBlocks = document.querySelectorAll('#label-middle > .field-block');
-  // console.log(midBlocks);
   let lastBlock = midBlocks[midBlocks.length - 1];
   lastBlock.parentNode.insertBefore(line, lastBlock.nextSibling);
   line.addEventListener('dragover', (e) => {
@@ -136,58 +123,11 @@ function addLine() {
   });
 }
 
-// Refactor this so that elements are created based on array of elements
-// function createPreviewEl(element, parent) {
-//   console.log(element);
-//   // console.log(element.field);
-//   // console.log(parent);
-//   let field = fieldProps[fieldProps.findIndex((x) => x.id === element.field)];
-//   // console.log(field);
-//   // Create parents first (if not available), then children inside
-//   let hasEl = preview.querySelector(`.${element.field}`) != null;
-//   // console.log(hasEl);
-//   if (!hasEl) {
-//     let newParentEl = document.createElement('div');
-//     newParentEl.classList.add(parent);
-//     preview.appendChild(newParentEl);
-//     let div = document.createElement('div');
-//     div.innerHTML = field.name;
-//     div.classList.add(field.id);
-//     div.classList.add(...element.className);
-//     newParentEl.appendChild(div);
-//   } else {
-//     // Check if child already exists div
-//     let hasItem =
-//       preview.querySelector(`.${parent} > .${element.field}`) != null;
-//     if (!hasItem) {
-//       let div = document.createElement('div');
-//       div.innerHTML = field.name;
-//       div.classList.add(field.id);
-//       div.classList.add(...element.className);
-//       preview.querySelector(`.${parent}`).appendChild(div);
-//     } else {
-//       // Clear div and append new
-//       preview.querySelector(`.${parent}`).innerHTML = '';
-//       let div = document.createElement('div');
-//       div.innerHTML = field.name;
-//       div.classList.add(field.id);
-//       div.classList.add(...element.className);
-//       preview.querySelector(`.${parent}`).appendChild(div);
-//     }
-//   }
-// }
 // Iterate throught every field block and add fields
 function createPreviewEl(element, parent) {
-  // console.log(element);
-  // console.log(document.querySelector(`#${element.field}`).parentNode);
-  // console.log(parent);
   // Grabs information from fieldProps array to create elements matching on id
   let fieldInfo =
     fieldProps[fieldProps.findIndex((x) => x.id === element.field)];
-  // console.log(fieldInfo);
-  //     let newParentEl = document.createElement('div');
-  //     newParentEl.classList.add(parent);
-  //     preview.appendChild(newParentEl);
   let div = document.createElement('div');
   div.innerHTML = fieldInfo.name;
   div.classList.add(fieldInfo.id);
@@ -197,8 +137,6 @@ function createPreviewEl(element, parent) {
   let hasSuffix = element.suffix != undefined;
   if (hasPrefix) {
     let currText = div.innerText;
-    // let prefSpan = document.createElement('span');
-    // prefSpan.innerText = element.prefix;
     let prefSpan = `<span>${element.prefix}</span>`;
     div.innerHTML = prefSpan + currText;
   }
@@ -209,45 +147,14 @@ function createPreviewEl(element, parent) {
   }
 }
 
-// const labelList = {
-//   'label-header': [],
-//   'label-middle': [],
-//   'label-footer': [],
-// };
-
 function isPrintStyle(className) {
   const functionalStyles = ['draggable', 'selected'];
   return !functionalStyles.includes(className);
 }
 
-// Add header and footer elements and format them (element is either head or foot)
-// function refreshFixedEl(elementName) {
-//   // Go through header area
-//   // get element
-//   let element = build.querySelector(`#label-${elementName} li`);
-//   // console.log(element);
-//   // get parent element
-//   let parent = build.querySelector(`#label-${elementName}`);
-//   // console.log(parent);
-//   // call create element
-//   // createPreviewEl(element, parent);
-//   // refreshPreview();
-//   // call update json
-//   // later need to allow heading elements to be clicked and formatted
-// }
-
 // Everytime label section is updated, refresh entire preview
-// Refactor to go only inside label-middle (eventually remove label-middle altogether)
 function refreshPreview() {
-  // let labelList = {};
-  // labelList is an array of fieldBlocks
   let labelList = [];
-  // console.log('---------------');
-  // console.log(`build has ${build.querySelectorAll('.draggable').length} items`);
-  // Go through every section in Label Middle
-  // const sections = build.querySelectorAll('.container');
-  // const sections = build.querySelectorAll('div');
-
   // Instead of sections, go through label-middle and gather fieldBlocks (lines)
   // itemsArr is an array of fields
   let fieldBlocks = document.querySelectorAll('#build-label .field-block');
@@ -270,70 +177,32 @@ function refreshPreview() {
     // console.log(labelList);
   });
 
-  // sections.forEach((section) => {
-  // console.log(`now in section ${section.id}`);
-
-  // let items = section.querySelectorAll('.draggable');
-  // let items = section.querySelectorAll('li');
-  // console.log(`${section.id} has ${items.length}`);
-
-  // Builds array based on sections
-  // labelList[section.id] = itemsArr;
-  // });
-  // console.log(labelList);
-
   // Clears preview div before appending elements
   preview.innerHTML = '';
 
-  // // Creates HTML elements and appends to preview div
-  // Object.keys(labelList).forEach((section) => {
-  //   labelList[section].forEach((item) => {
-  //     // createPreviewEl(item, section);
-  //     createPreviewEl(item);
-  //   });
-  // });
+  // Creates HTML elements and appends to preview div
   labelList.forEach((labelItem) => {
-    // console.log(labelItem);
-    // create fieldBlock div
     let fieldBlock = document.createElement('div');
     fieldBlock.classList.add('field-block');
     preview.appendChild(fieldBlock);
-    // create elements inside fieldBlock
     labelItem.forEach((field) => {
       createPreviewEl(field, fieldBlock);
     });
-    // createPreviewEl(fieldBlock);
   });
 
   generateJson(labelList);
 }
 // Generate JSON string for current configurations
 function generateJson(list) {
-  // console.log(labelList);
-  // let json = JSON.stringify(labelList);
-  // let labelFormat = {};
-  // labelFormat.title = 'Label Format Title';
-  // labelFormat.displaySpeciesAuthor = 0;
-  // labelFormat.displayBarcode = 0;
-  // labelFormat.labelType = '2';
-  // labelFormat.defaultStyles = 'font-size:8pt';
-  // labelFormat.defaultCss = '../../css/symb/labelhelpers.css';
-  // labelFormat.customCss = '';
-  // labelFormat.labelDiv = { className: 'label-md' };
-  // labelFormat.labelBlocks = [labelList['label-header']];
-
   // "list" is an array of fields --> fieldBlock
   // place each "list" inside a fieldBlock
   // then place all fieldBlocks in an array
 
   // Each section in labelList should be translated into a
   // divBlock, where the className should include the id
-  // let divBlocks = [];
   let labelBlocks = [];
   // Parse nested array
   Object.keys(list).forEach((index) => {
-    // console.log(list[index]);
-    // console.log(index);
     let fieldBlockObj = {};
     // Joins array of className items
     let fieldItem = list[index];
@@ -347,48 +216,15 @@ function generateJson(list) {
     fieldBlockObj.fieldBlock = fieldItem;
     labelBlocks.push(fieldBlockObj);
     console.log(fieldBlockObj);
-    // let fields = [];
-    // fields.push(list[index]);
   });
   console.dir(labelBlocks);
 
-  // Object.keys(list).forEach((section) => {
-  //   let hasItems = list[section].length > 0;
-  //   if (hasItems) {
-  //     let fieldBlock = {};
-  //     let fields = [];
-  //     divBlocks.push({
-  //       // divBlock: { className: section, blocks: [fieldBlock] },
-  //       divBlock: { blocks: [fieldBlock] },
-  //     });
-  //     // console.log(divBlocks);
-  //     // console.log(section);
-  //     list[section].forEach((item) => {
-  //       // console.log(item);
-  //       let field = {};
-  //       field.field = item.field;
-  //       item.className ? (field.className = item.className.join(' ')) : '';
-  //       // console.log(field);
-  //       item.prefix ? (field.prefix = item.prefix) : '';
-  //       item.suffix ? (field.suffix = item.suffix) : '';
-  //       fields.push(field);
-  //       fieldBlock.fieldBlock = fields;
-  //       // console.log(fields);
-  //     });
-  //   }
-  // });
-  // labelFormat.labelBlocks = divBlocks;
-  // let json = JSON.stringify(labelFormat);
-  // let json = JSON.stringify(divBlocks);
   let json = JSON.stringify(labelBlocks);
   console.log(json);
 }
 
 function toggleSelect(element) {
-  // console.log('element ' + element.target);
-  // console.log('is selected before? ' + element.classList.contains('selected'));
   element.classList.toggle('selected');
-  // console.log('is selected now? ' + element.classList.contains('selected'));
   return element.classList.contains('selected');
 }
 
@@ -404,66 +240,39 @@ function activateControls(bool) {
  * @param {*} item item to show applied formats
  */
 function getState(item) {
-  // console.log('item is selected: ' + item.classList.contains('selected'));
   let formatList = Array.from(item.classList);
-  // console.log(item.id + ' has ' + formatList);
   // Removes '.draggable' and '.selected' from array
   formatList.splice(formatList.indexOf('selected'), 1);
   formatList.splice(formatList.indexOf('draggable'), 1);
-  // console.log(item.id + ' has ' + formatList);
-  // console.log(formatList.length);
   if (formatList.length > 0) {
-    // let group = new RegExp(`${control.id}-*`);
-    // let hasGroup = formatItem.className.split(' ').some(function (c) {
-    //   return group.test(c);
-    // });
-
     // Render state of each formatting button
     formatList.forEach((formatItem) => {
-      // console.log('formatItem: ' + formatItem);
       // Check if class is a choice in a dropdown by matching first part of class
       let strArr = formatItem.split('-');
       let str = strArr[0];
       // Loop through each item in array
       dropdownsArr.forEach((dropdown) => {
         let isDropdownStyle = str === dropdown.id;
-        // console.log(strArr[0]);
-        // console.log(dropdowns.id);
-        // console.log('is style in a dropdown? ' + isDropdownStyle);
         if (isDropdownStyle) {
           let selDropdown = document.getElementById(str);
           selDropdown.value = formatItem;
         }
       });
-      // else if (fomatItem === dropdowns.id)
       controls.forEach((control) => {
         // Select that format and activate it
         if (formatItem === control.dataset.func) {
           control.classList.add('selected');
         }
-        // else if (formatItem === control.value) {
-        //   console.log('select');
-        //   control.value = formatItem;
-        // }
-        // console.log(formatItem + ' is active');
       });
     });
   }
-  // else {
-  //   // controls.forEach((control) => {
-  //   //   control.classList.remove('selected');
-  //   // });
-  // resetControls();
-  // }
 
   // Get state of prefix/suffix for fields
   let hasPrefix = item.dataset.prefix != null;
   let prefixInput = document.getElementById('prefix');
-  // console.log(hasPrefix);
   hasPrefix ? (prefixInput.value = item.dataset.prefix) : '';
   let hasSuffix = item.dataset.suffix != null;
   let suffixInput = document.getElementById('suffix');
-  // console.log(hasPrefix);
   hasSuffix ? (suffixInput.value = item.dataset.suffix) : '';
 }
 
@@ -476,37 +285,10 @@ function getState(item) {
 function toggleStyle(control, selectedItems, bool) {
   // Toggles class in item - causes errors when multiple are selected
   // with conflicting styles...
-  // formatItems.forEach((item) => {
-  //   item.classList.toggle(control.dataset.func);
-  //   // item.dataset.classes.toggle(control.dataset.func);
-  // });
-  // console.log(formatItems);
-  // if (isSelected) {
-  //   formatItems.forEach((item) => {
-  //     item.classList.add(control.dataset.func);
-  //     // console.log('added to classList: ' + control.dataset.func);
-  //   });
-  // } else {
-  //   formatItems.forEach((item) => {
-  //     item.classList.remove(control.dataset.func);
-  //     // console.log('removed from classList: ' + control.dataset.func);
-  //   });
-  // }
-  // make sure we're getting only selected items
-  // console.log(selectedItems);
   selectedItems.forEach((item) => {
-    // console.log(`item ${item.id} will be formatted? ${bool}`);
-    // console.log(bool);
-    // console.log(item);
     // Double-checking if item is selected
     if (item.classList.contains('selected')) {
-      // console.log(
-      //   `item ${item.id} is selected ${item.classList.contains('selected')}`
-      // );
-
       // Deals with buttons
-
-      // console.log('item ' + item.id + ' is selected');
       // if formatting button is selected, add class, else remove
       bool
         ? item.classList.add(control.dataset.func)
@@ -516,7 +298,6 @@ function toggleStyle(control, selectedItems, bool) {
       return false;
     }
     refreshPreview();
-    // console.log('inside toggleStyle');
   });
 }
 
@@ -527,8 +308,6 @@ function toggleStyle(control, selectedItems, bool) {
  */
 function addReplaceStyle(dropdown, selectedItems) {
   // Deals with selection
-  // console.log('selected items ' + selectedItems);
-  // console.dir(selectedItems);
   dropdown.addEventListener('input', function () {
     selectedItems.forEach((item) => {
       let option = dropdown.value;
@@ -542,9 +321,7 @@ function addReplaceStyle(dropdown, selectedItems) {
           if (!hasGroup) {
             // If not, add it
             item.classList.add(option);
-            // item.classList.add('added-if-no-group');
             console.log(`added ${option} to ${item.id}`);
-            // console.log(selectedItems);
           } else {
             // If yes, replace it
             item.classList.forEach((className) => {
@@ -553,14 +330,11 @@ function addReplaceStyle(dropdown, selectedItems) {
               }
             });
             item.classList.add(option);
-            // console.log('added-after-removing-group');
           }
         }
       }
     });
   });
-  //
-  // console.dir(selectedItems);
   refreshPreview();
 }
 
@@ -596,17 +370,12 @@ function handleDrop(e) {
   if (dragSrcEl != this) {
     this.parentNode.insertBefore(dragSrcEl, this);
   }
-  // refreshPreview();
   return false;
 }
 
 function handleDragEnd(e) {
-  // e.preventDefault();
   this.classList.remove('dragging');
-  // refreshPreview(e.target.parentNode.id);
   refreshPreview();
-  // console.log('inside handleDragEnd');
-  // createPreviewEl(e.target.id, e.target.parentNode.id);
   return false;
 }
 
@@ -631,24 +400,18 @@ containers.forEach((container) => {
 // Elements in '#build-label' that are formattable
 build.addEventListener('click', (e) => {
   if (e.target && e.target.matches('.draggable')) {
-    // console.log(e.target.id);
-    // getState(e.target);
-    // formatMenu(e);
     let isSelected = toggleSelect(e.target);
     // Resets formatting buttons state when item is deselected
     !isSelected ? resetControls() : '';
-    // console.log(isSelected);
     // When element is selected, activate formatting buttons
     // depends on number of elements in page (at least one selected).
     let anySelected = build.querySelectorAll('.selected').length > 0;
     activateControls(anySelected);
     let numSelected = build.querySelectorAll('.selected');
-    // console.log(numSelected.length + ' items selected');
     // Gets formatting information for individually selected item
     if (numSelected.length > 1) {
       resetControls();
     } else if (numSelected.length == 1) {
-      // console.log(numSelected.length);
       // Refreshes buttons according to applied styles in selected item
       let item = build.querySelector('.selected');
       getState(item);
@@ -662,28 +425,13 @@ build.addEventListener('click', (e) => {
 controlDiv.addEventListener('click', (e) => {
   // Gets selected items to format
   let formatItems = build.querySelectorAll('li.selected');
-  // console.log(formatItems);
-  // if (e.target && e.target.matches('.control')) {
-  //   // formatMenu(e);
   let isFormatSelected = toggleSelect(e.target);
   let isButton = e.target.tagName === 'BUTTON';
   let isDropdown = e.target.tagName === 'SELECT';
-  // let isInput = e.target.tagName === 'INPUT';
-  // console.log(e.target.tagName);
-  // console.log(isButton, isDropdown);
-  // Apply styles in item
-  // console.log(
-  //   'botão ' +
-  //     e.target.dataset.func +
-  //     ' e item selecionado ' +
-  //     document.querySelector('.draggable.selected').id
-  // );
   // Buttons
   if (isButton) {
-    // console.log('is a buton');
     toggleStyle(e.target, formatItems, isFormatSelected);
   } else if (isDropdown) {
-    // console.log('is a dropdown');
     addReplaceStyle(e.target, formatItems);
   }
 });
@@ -699,32 +447,7 @@ inputs.forEach((input) => {
   });
 });
 
-// getState(e.target);
-// }
-// Get state of control for selected item(s)
-// let itemHasStyles = getState(e.target).length > 0;
-// console.log(itemHasStyles);
-
 function updateInputVal(el, item) {
   let option = el.id;
-  // console.log(option);
-  // console.log(item);
   item.setAttribute('data-' + option, el.value);
 }
-
-//  On load
-// refreshFixedEl('header');
-
-// ************** TO DO ************
-// - [x] Need to reset controls for when more than one item is selected
-// - [x] Load state to controls depending on selected item styles
-// - [x] Update preview styles
-// - [x] Capture styles in object array
-// - [x] Translate object array into JSON in the appropriate format
-// - [] Add more format buttons
-// --[x] Underline
-// --[x] Uppercase
-// --[] Font-type
-// --[] Font-size
-// --[] Spacing
-// --[] Bar option?
